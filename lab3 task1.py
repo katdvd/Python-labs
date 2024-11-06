00ba22e343cbb431756089f6f994c94fbe32b81e
katdvd <katyadddddv@gmail.com>
2024-11-06 20:47:33 +0300

    лаб раб 3

---
+++ lab3 task1.py
@@ -0,0 +1,15 @@
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+
+# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+
+money_capital = 0
+for i in range(months):
+    money_capital += spend*(((increase+1)**i)) - salary
+
+# if money_capital/int(money_capital) != 0:
+#     money_capital += 1
+
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", int(money_capital))
