2e9de17bd35d950184e45921f2017cbe2a95a77f
katdvd <katyadddddv@gmail.com>
2024-11-11 02:06:50 +0300

    лаб раб 4

---
+++ lab4 task2.py
@@ -0,0 +1,30 @@
+# TODO импортировать необходимые молули
+import json
+import csv
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+
+def task() -> None:
+    x = open(INPUT_FILENAME, "r")
+    y = open(OUTPUT_FILENAME, "w")
+    reader = csv.DictReader(x)
+    z = []
+    for i in reader:
+        z.append(i)
+    # TODO считать содержимое csv файла
+
+    json.dump(z, y, indent=4)
+    y.close()
+    x.close()
+    # TODO Сериализовать в файл с отступами равными 4
+
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
